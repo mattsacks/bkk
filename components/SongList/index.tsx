9e65643e29d2e387b9ec5ed55c2602c3d1651af9
Matt Sacks <github@mattsacks.email>
2019-11-07 15:55:46 -0800

    Add <SongListItem>, prevents <SongList> from re-rendering on queue add

--- components/SongList/index.tsx
+++ components/SongList/index.tsx
@@ -1,18 +1,7 @@
-import React, { useCallback, useMemo, useState } from "react";
-import classnames from "classnames";
-import SVG from "react-inlinesvg";
-import request from "lib/request";
+import React, { useMemo } from "react";
 import { Song } from "lib/types";
+import SongListItem from "components/SongListItem";
 import styles from "./styles.scss";
-
-async function addToQueue(song: Song) {
-  await request("tracks", {
-    body: {
-      song_id: song.id
-    },
-    method: "POST"
-  });
-}
 
 function groupSongsByArtist(songs: Song[]) {
   return songs.reduce((grouped, song: Song) => {
@@ -28,57 +17,18 @@ function groupSongsByArtist(songs: Song[]) {
 
 function SongList(props: { songs: Song[] }) {
   const { songs } = props;
-  const [addedSongIds, setAddedSongIds] = useState([]);
 
   const groupedSongsByArtist = useMemo(() => groupSongsByArtist(songs), [
     songs
   ]);
-
-  const onSongClick = useCallback(
-    (song) => {
-      return (e) => {
-        addToQueue(song);
-        setAddedSongIds((prevAddedSongIds) => [...prevAddedSongIds, song.id]);
-
-        // Remove the song from list of added songs after a timeout
-        setTimeout(() => {
-          setAddedSongIds((prevAddedSongIds) => {
-            return prevAddedSongIds.filter((songId) => songId !== song.id);
-          });
-        }, 2500);
-      };
-    },
-    [songs]
-  );
 
   const songsByArtist = [];
   for (const artist in groupedSongsByArtist) {
     const songElements = [];
 
     for (const song of groupedSongsByArtist[artist]) {
-      const isAddedSong = addedSongIds.includes(song.id);
-
-      // FIXME don't re-create all of these buttons on each render 😓
       songElements.push(
-        <button
-          className={styles.songButton}
-          disabled={isAddedSong}
-          key={song.id}
-          onClick={onSongClick(song)}
-        >
-          <div
-            className={classnames(styles.song, {
-              [styles.addedToQueue]: isAddedSong
-            })}
-          >
-            {song.name}
-          </div>
-          {isAddedSong ? (
-            <div>queued!</div>
-          ) : (
-            <SVG className={styles.songIcon} src="/right-chevron.svg" />
-          )}
-        </button>
+        <SongListItem key={song.id} song={song} />
       );
     }
 
