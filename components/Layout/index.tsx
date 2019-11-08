3cd28e222535e86346386a826af9f4caddf0c085
Matt Sacks <github@mattsacks.email>
2019-11-07 22:01:15 -0800

    Fix for Firefox

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -5,7 +5,7 @@ import styles from "./styles.scss";
 export default function Layout(props) {
   return (
     <div className={styles.backgroundContainer}>
-      <div className={styles.background} />
+      <div className={styles.background}></div>
       <main className={styles.layout}>
         {props.children}
       </main>
