e97d135aca378b2d345a822be66435d484982ec7
Matt Sacks <github@mattsacks.email>
2019-11-08 17:22:05 -0800

    Show current user's songs in the queue

--- components/QueueItem/index.tsx
+++ components/QueueItem/index.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import classnames from "classnames";
+import useLoggedIn from "lib/useLoggedIn";
 import styles from "./styles.scss";
 
 function QueueItem({ index, queue }) {
   const indexText = index === 1 ? "up next" : index;
+  const { user } = useLoggedIn()[0];
 
   return (
-    <div className={styles.queueItem}>
+    <div
+      className={classnames(styles.queueItem, {
+        [styles.mySong]: user.userName === queue.user_name
+        // James should go to jail for this right here
+      })}
+    >
       <span className={styles.index}>{indexText}:</span>
       {queue.song_name} by {queue.artist}
       <span className={styles.user}>({queue.user_name})</span>
     </div>
-  )
+  );
 }
 
 export default QueueItem;
