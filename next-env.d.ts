4556b802bd3d99622732b51ab67e95f30a5d1784
Matt Sacks <github@mattsacks.email>
2022-04-30 14:16:00 -0700

    Upgrade next and typescript

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,12 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare module "*.scss" {
-  const content: { [className: string]: string };
-  export = content;
-}
-
-declare module "*.svg" {
-  const content: unknown;
-  export default content;
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
